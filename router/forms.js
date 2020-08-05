ad4efdd3336497f5675a99429846eefa606b1888
Haiyang Hong <hong961127@gmail.com>
2020-08-05 15:37:51 -0700

    Delete controllers for previous REST implementation

--- router/forms.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const { createFormAuth, createFormGuest } = require("../controller/forms");
-const auth = require("../middlewares/auth");
-
-router.route("/guest").post(createFormGuest);
-router.route("/auth").post(auth, createFormAuth);
-
-module.exports = router;
